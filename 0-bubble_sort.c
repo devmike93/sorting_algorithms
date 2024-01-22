6b281dd62c152b6b55a3f3709649b9051583495e
Michael Megbenu <msk.megbenu@gmail.com>
2024-01-22 02:34:32 +0200

    bubble correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,14 +10,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swapped;
 
 	if (!array || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -28,15 +26,8 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-				/* Set swapped flag */
-				swapped = 1;
 			}
 		}
-
-		/* If no two elements were swapped in inner loop, the array is sorted */
-		if (!swapped)
-			break;
-
 		/* Print the array after each pass */
 		print_array(array, size);
 	}
